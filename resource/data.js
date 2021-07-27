c0a3d5db981eb1f4c812e86cb643c8b22ae9cb6d
ikess <ikess0330@iris.snu.ac.kr>
2021-07-27 09:55:11 +0900

    add resource+
    Signed-off-by: ikess <ikess0330@iris.snu.ac.kr>

---
+++ resource/data.js
@@ -0,0 +1,50 @@
+{
+  "season1": {
+    "play_list_id": "PL60xc-Khy-RkATUwfmp0HZmXo37DjilAD",
+    "video_ids": [
+      {"id": "SB1QslVtedo"},
+      {"id": "9J4aph6dU3E"},
+      {"id": "7Fc9sf2YLuQ"},
+      {"id": "OWPQlTzZa_I"},
+      {"id": "aKhws0joxDA"},
+      {"id": "KDfVbAxvFqQ"},
+      {"id": "Sr8fdo6ZOiY"},
+      {"id": "KOTFx7-o_LA"},
+      {"id": "N-g9RzGcqUU"},
+      {"id": "aYDuaY_LnYQ"},
+      {"id": "FYM87iBY0m8"},
+      {"id": "cwh2nQGO1No"}
+    ]
+  },
+  "season2" : {
+    "play_list_id": "PL60xc-Khy-Rk_D63NojPd7ZJ72-KkSlNL",
+    "video_ids": [
+      {"id": "HBKhn-lpBAI"},
+      {"id": "QVUr5-a73Ps"},
+      {"id": "qndpZP_n3Ew"},
+      {"id": "teFn06zlCeA"},
+      {"id": "Z0PrG1H-Kf0"},
+      {"id": "s9DGEUHubVw"},
+      {"id": "eCcMdnq3sqQ"},
+      {"id": "x0n3d4Y-Q_Y"},
+      {"id": "G_3QavPY_Ek"},
+      {"id": "xvDMZXh8_yI"},
+      {"id": "zgIVhGIJriE"},
+      {"id": "t8-oJ6YDEtw"}
+    ]
+  },
+  "season3" : {
+    "play_list_id": "PL60xc-Khy-RkY5uge1cDIjtq1Hx_F5YDu",
+    "video_ids": [
+      {"id": "LGD_VAZraWk"},
+      {"id": "DOBG7X3MVi8"},
+      {"id": "pNLMqiWHNqk"},
+      {"id": "HC7VDyS4DIo"},
+      {"id": "fFt-3AjaNwU"},
+      {"id": "k23H8APVLzY"},
+      {"id": "dPJkyL3dDuY"},
+      {"id": "cVP77ALLHb8"},
+      {"id": "ifTULoErW5s"}
+    ]
+  }
+}
